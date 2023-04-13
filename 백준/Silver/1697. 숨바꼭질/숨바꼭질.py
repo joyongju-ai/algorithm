6968c126aa7597c1ce07fccc014ba084d8fc17c1
joyongju-ai <111257779+joyongju-ai@users.noreply.github.com>
2023-04-13 14:03:42 +0900

    [Silver I] Title: 숨바꼭질, Time: 184 ms, Memory: 118780 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,29 +1,25 @@
+#내 식대로 개조한 코드
+#즉석 리스트와 visted 배제에 초점
+import sys
 from collections import deque
-n,k=map(int,input().split())
+
+MAX=10**5
 INF=int(1e9)
-visited=[False]*(100001)
-distance=[INF]*(100001)
+distance=[INF]*(MAX+1)
+n,k=map(int,sys.stdin.readline().split())
 
-q=deque([n])
-visited[n]=True
+q=deque()
+q.append(n)
 distance[n]=0
-
 while q:
-  now=q.popleft()
+    x=q.popleft()
+    #즉석리스트
+    for nx in (x-1,x+1,x*2):
+        #distance 변형으로 방문처리 대신하기,visited 배제
+        if 0 <= nx <= MAX and distance[nx]==INF:
+            distance[nx]=distance[x]+1  
+            q.append(nx)
+    if distance[k]!=INF:
+        break
 
-  if now+1<100001 and not visited[now+1]:
-    q.append(now+1)
-    distance[now+1]=distance[now]+1
-    visited[now+1]=True
-  if now-1>=0 and not visited[now-1]:
-    q.append(now-1)
-    distance[now-1]=distance[now]+1
-    visited[now-1]=True
-  if now*2<100001 and not visited[now*2]:
-    q.append(now*2)
-    distance[now*2]=distance[now]+1
-    visited[now*2]=True
-  if visited[k]:
-    break
-    
 print(distance[k])
