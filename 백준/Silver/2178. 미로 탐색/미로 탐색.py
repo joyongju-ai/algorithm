ee23088ebf70e5ce736402ced32a80e0bfb50629
joyongju-ai <111257779+joyongju-ai@users.noreply.github.com>
2025-10-17 14:07:02 +0900

    [Silver I] Title: 미로 탐색, Time: 124 ms, Memory: 114460 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,46 +1,41 @@
+#최단 경로는 bfs
+import sys
 from collections import deque
-#입력부
-n,m=map(int,input().split())
+input = sys.stdin.readline
 
-#field 입력받기,공백으로 나눠주지 않았으므로 별도 처리
-field=[]
+n,m = list(map(int,input().strip().split())) #readline에는 개행문자 들어감에 주의
+maze = []
 for i in range(n):
-  array=input()
-  array=" ".join(array)
-  field.append(list(map(int,array.split())))
+    temp = list(map(int,input().strip()))
+    maze.append(temp)
 
-#visited 초기화,start 초기화
-visited=[[False]*(m) for i in range(n)]
-start=(0,0)
+visited=[[False]*m for _ in range(n)]
 
-#방향벡터
-dx=[1,0,-1,0]
-dy=[0,1,0,-1]
+count_map = [[0]*m for _ in range(n)] #
+start =(0,0)
+goal = (n,m)
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
 
-
-#bfs
-def bfs(field,start,visited):
-  q=deque([start])                  
-  visited[start[0]][start[1]]=True
-  
-  while q:
-    x,y=q.popleft()
-    #4방향 살펴보며
-    for i in range(4):
-      nx=x+dx[i]
-      ny=y+dy[i]
-      #미로를 벗어나지 않고
-      if 0<=nx<n and 0<=ny<m:
-        #미방문인 곳 중 field의 값이 1인 곳이면
-        if field[nx][ny]==1 and not visited[nx][ny]:
-          #field값을 이동 이전 노드의 값보다 1 높은 값으로 갱신
-          field[nx][ny]=field[x][y]+1
-          q.append((nx,ny))
-          visited[nx][ny]=True
-    #n-1,m-1에 도달하면 종료,최단거리 구하기 위해     
-    if visited[n-1][m-1]==True:
-      break
-  return field[n-1][m-1]
-
-
-print(bfs(field,start,visited))
+def bfs(maze, start, visited, goal ):
+    queue = deque([start])
+    visited[0][0] = True
+    count_map[0][0] = 1
+    n,m = goal
+    while(queue):
+        x,y = queue.popleft()
+        for i in range(4):
+            nx = x +dx[i]
+            ny = y +dy[i]
+            if nx < 0 or nx >= n or ny <0 or ny >= m:
+                continue
+            if not visited[nx][ny] and maze[nx][ny] == 1:
+                queue.append((nx,ny))
+                visited[nx][ny] = True
+                count_map[nx][ny] = count_map[x][y] + 1 #
+        if visited[n-1][m-1]:
+            break
+    return count_map[n-1][m-1]               
+    
+result = bfs(maze, start, visited, goal)
+print(result)
