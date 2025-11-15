5edce2dae55d45670a8b85a2090dcc7331700026
joyongju-ai <111257779+joyongju-ai@users.noreply.github.com>
2025-11-15 23:40:25 +0900

    [Silver I] Title: 숨바꼭질, Time: 124 ms, Memory: 112796 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,25 +1,20 @@
-#내 식대로 개조한 코드
-#즉석 리스트와 visted 배제에 초점
-import sys
 from collections import deque
+n,k = map(int,input().split())
 
-MAX=10**5
-INF=int(1e9)
-distance=[INF]*(MAX+1)
-n,k=map(int,sys.stdin.readline().split())
+q = deque([n])
+dist= [-1]*100001
+dist[n] = 0
 
-q=deque()
-q.append(n)
-distance[n]=0
 while q:
-    x=q.popleft()
-    #즉석리스트
-    for nx in (x-1,x+1,x*2):
-        #distance 변형으로 방문처리 대신하기,visited 배제
-        if 0 <= nx <= MAX and distance[nx]==INF:
-            distance[nx]=distance[x]+1  
-            q.append(nx)
-    if distance[k]!=INF:
-        break
+    x = q.popleft()
 
-print(distance[k])
+    if x == k:
+        print(dist[k])
+        break
+    for dx in [1,-1, x]:
+        nx = x + dx
+        if nx < 0 or nx > 100000:
+            continue
+        if dist[nx] == -1:
+            dist[nx] = dist[x] + 1
+            q.append(nx)
