0a1a1c7e220a51c3abd6987c131f8b251f1fe7b9
joyongju-ai <111257779+joyongju-ai@users.noreply.github.com>
2025-11-16 18:29:52 +0900

    [Silver I] Title: 안전 영역, Time: 228 ms, Memory: 120720 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,48 +1,40 @@
 from collections import deque
-from copy import deepcopy
-n=int(input())
 
-field=[]
-for i in range(n):
-  field.append(list(map(int,input().split())))
-copyfield=deepcopy(field)
+n = int(input())
+space = [list(map(int,input().split())) for _ in range(n)]
 
-max_height=0
-min_height=101
-for i in range(n):
-  for j in range(n):
-    max_height=max(max_height,field[i][j])
-    min_height=min(min_height,field[i][j])
+heights = []
+for row in range(n):
+    for col in range(n):
+        if space[row][col] not in heights:
+            heights.append(space[row][col])
 
-def bfs(start,height):
-  q=deque([start])
-  dx=[1,0,-1,0]
-  dy=[0,1,0,-1]
-  
-  while q:
-    x,y=q.popleft()
-    for i in range(4):
-      nx=x+dx[i]
-      ny=y+dy[i]
-      if 0<=nx<n and 0<=ny<n:
-        if field[nx][ny]>height:
-          field[nx][ny]=0
-          q.append((nx,ny))
+q= deque()
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
 
+max_count = 1
 
-count=0
-result=[]
-for k in range(min_height,max_height+1):
-  for i in range(n):
-    for j in range(n):
-      if field[i][j]>k:
-        count+=1
-        bfs((i,j),k)
-  result.append(count)
-  count=0
-  field=deepcopy(copyfield)
+for h in heights:
+    visited = [[0]*n for _ in range(n)]
+    count = 0
+    for i in range(n):
+        for j in range(n):
+            if space[i][j] > h and visited[i][j] == 0:
+                q.append((i,j))
+                visited[i][j] = 1
+                count += 1
+                while q:
+                    x,y = q.popleft()
 
-if max(result)==0:
-  print(1)
-else:
-  print(max(result))
+                    for k in range(4):
+                        nx, ny = x + dx[k], y + dy[k]
+
+                        if nx < 0 or nx >= n or ny <0 or ny >= n:
+                            continue
+                        if space[nx][ny] > h and visited[nx][ny] == 0:
+                            visited[nx][ny] = 1
+                            q.append((nx,ny))
+    max_count = max(max_count, count)
+
+print(max_count)
