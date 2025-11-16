2bcd4a27e38d893d09dfd0a8f0b2c149fc6492de
joyongju-ai <111257779+joyongju-ai@users.noreply.github.com>
2025-11-17 01:46:57 +0900

    [Gold IV] Title: 빙산, Time: 460 ms, Memory: 144372 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,93 +1,73 @@
 from collections import deque
 
-n,m=map(int,input().split())
 
-graph=[]
-next=deque()  #해당하는 빙하만 넣는 큐@
-for i in range(n):
-  graph.append(list(map(int,input().split())))
-  #graph 중에 0이 아닌 것만 next에 담음
-  for j in range(m):
-    if graph[i][j]!=0:
-      next.append((i,j,graph[i][j]))
-
-#방향벡터
-dx=[0,1,0,-1]
-dy=[1,0,-1,0]
-
-
-#bfs 빙하의 4방향을 살피고 count를 0의 개수만큼 빼준다
-#1년 단위의 빙하 변화 실행
 def bfs():
-  q=deque()
-  delete=[]
-  #next의 값을 꺼내 q에 넣기
-  while next:
-    x,y,count=next.popleft()
-    q.append((x,y,count))
+    visited = [[0] * m for _ in range(n)]
+    count = 1
+    q = deque()
+    q.append(ice[0])
+    visited[ice[0][0]][ice[0][1]] = 1
+    while q:
+        x,y = q.popleft()
 
-  #q가 빌 때까지
-  while q:
-    x,y,count=q.popleft()
-  
-    for i in range(4):
-      nx,ny=x+dx[i],y+dy[i]
-      if not(0<=nx<n and 0<=ny<m):
-        continue
-      #0일 때 count 줄임
-      if graph[nx][ny]==0:
-        count-=1
-        #count0이면 graph[x][y]갱신 
-      if count==0:
-        delete.append((x,y))
+        for i in range(4):
+            nx,ny = x + dx[i], y + dy[i]
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            if space[nx][ny] != 0 and visited[nx][ny] == 0:
+                visited[nx][ny] = 1
+                q.append((nx,ny))
+                count += 1
+    #print(count,len(ice))
+    if count != len(ice):
+        return 1
+    else:
+        return 0
+
+
+n,m = map(int,input().split())
+space = [list(map(int,input().split())) for _ in range(n)]
+
+
+q = deque()
+ice = []
+for i in range(n):
+    for j in range(m):
+        if space[i][j] != 0:
+            ice.append((i,j))
+
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
+
+year = 0
+while True:
+    year += 1
+    decrease = []
+    for x,y in ice:
+        dec = 0
+        for i in range(4):
+            nx,ny = x + dx[i], y + dy[i]
+            if space[nx][ny] == 0:
+                dec += 1
+        decrease.append(dec)
+    #print(decrease)
+    for i in range(len(ice)):
+        space[ice[i][0]][ice[i][1]] -= decrease[i]
+        if space[ice[i][0]][ice[i][1]] <= 0:
+            space[ice[i][0]][ice[i][1]] = 0
+    for i in range(len(ice)-1,-1,-1):
+        if space[ice[i][0]][ice[i][1]] == 0:
+            #print(ice[i][0],ice[i][1])
+            ice.pop(i)
+    #print(ice)
+    if ice:
+        result = bfs()
+        if result == 1:
+            print(year)
+            break
+        #else:
+        #   print("실패")
+    else:
+        print(0)
         break
-    #count가 0보다 큰 것만 next에 담기
-    #q에 담으면 1년마다 덩어리 수 확인하기 어려우므로 next 활용
-    if count>0:
-      next.append((x,y,count)) 
-  for i,j in delete:
-    graph[i][j]=0
-
-    
-#빙하 덩어리 분리
-def bfs2(x,y):
-  queue=deque([(x,y)])
-  visited[x][y]=True
-
-  while queue:
-    x,y=queue.popleft()
-    for i in range(4):
-      nx,ny=x+dx[i],y+dy[i]
-      if not(0<=nx<n and 0<=ny<m):
-        continue
-      if visited[nx][ny] or graph[nx][ny]==0:
-        continue
-      queue.append((nx,ny))
-      visited[nx][ny]=True
-
-
-years=0    #시간
-#next가 빌 때까지
-while next:
-  ice_count=0  #덩어리 수 1년마다 초기화
-  visited=[[False]*(m) for _ in range(n)] #visited 갱신
-
-  #빙하 덩어리 수 세기
-  for i in next:
-    x,y,count=i
-    #next에서 꺼낸 좌표중 미방문인 것만
-    if not visited[x][y]:
-      bfs2(x,y)   #덩어리 분리
-      ice_count+=1
-  #2개 이상으로 나눠지면 종료
-  if ice_count>1:
-    break
-
-  years+=1  #bfs 시행할 때마다 1년 추가
-  bfs()
-  
-  
-if ice_count>1:
-  print(years)
-else:
-  print(0)
