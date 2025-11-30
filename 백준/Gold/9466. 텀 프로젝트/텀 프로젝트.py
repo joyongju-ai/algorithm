ed2aeae7b266e7d6a0e19cfb2a24a2d67f1c74ad
joyongju-ai <111257779+joyongju-ai@users.noreply.github.com>
2025-11-30 18:13:20 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1260 ms, Memory: 391288 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -1,36 +1,30 @@
 import sys
-sys.setrecursionlimit(10**5)
+sys.setrecursionlimit(10**5)   # 재귀깊이 996에서 10만까지 확대
 
 def dfs(x):
     global cnt
     visited[x] = True
+    team.append(x)
     nx = arr[x]
-
-    if not visited[nx]:
+    
+    if visited[nx]:
+        if nx in team:
+            cnt += len(team[team.index(nx):])
+    else:
         dfs(nx)
-    elif not finished[nx]:
-        # 사이클 발견
-        cur = nx
-        cycle_len = 1
-        while arr[cur] != nx:
-            cur = arr[cur]
-            cycle_len += 1
-        cnt += cycle_len
-
-    finished[x] = True
 
 
-T = int(sys.stdin.readline())
+T = int(input())
 for _ in range(T):
-    n = int(sys.stdin.readline())
-    arr = [0] + list(map(int, sys.stdin.readline().split()))
+    n = int(input())
+    arr = [0] + list(map(int, input().split()))
 
-    visited = [False] * (n+1)
-    finished = [False] * (n+1)
+    visited = [False] * (n+1)       #방문 여부 확인
     cnt = 0
 
     for i in range(1, n+1):
         if not visited[i]:
+            team = []
             dfs(i)
 
     print(n - cnt)
