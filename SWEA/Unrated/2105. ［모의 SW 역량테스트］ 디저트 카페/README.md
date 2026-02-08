fc76938827bf074a552c9a857694dbc9969f883a
joyongju-ai <111257779+joyongju-ai@users.noreply.github.com>
2026-02-08 14:58:41 +0900

    [Unrated] Title: [모의 SW 역량테스트] 디저트 카페, Time: 199 ms, Memory: 5,992 KB -BaekjoonHub

---
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/README.md
@@ -0,0 +1,15 @@
+# [Unrated] [모의 SW 역량테스트] 디저트 카페 - 2105 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5VwAr6APYDFAWu) 
+
+### 성능 요약
+
+메모리: 5,992 KB, 시간: 199 ms, 코드길이: 1,792 Bytes
+
+### 제출 일자
+
+2026-02-08 14:58
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<algorithm>
+
+using namespace std;
+
+int visited[21][21];
+int visited_value[105];
+int MAP[21][21];
+
+int dx[4] = { 1,-1,-1,1 };
+int dy[4] = { 1,1,-1,-1 };
+
+int N;
+int max_cnt;
+
+struct Point {
+	int y, x;
+};
+
+Point st;
+
+
+void dfs(Point cp, int dir, int cost,int st_val, int rest_turn) {
+	if (rest_turn < 0) {
+		return;
+	}
+	if (cp.x == st.x && cp.y == st.y && cost > 0) {
+		max_cnt = max(max_cnt, cost);
+		//cout << cost << "#" << "\n";
+		return;
+	}
+
+
+	for (int i = 0; i < 2; i++) {
+		Point np = { cp.y + dy[(dir + i)%4], cp.x + dx[(dir + i)%4] };
+
+		if (np.y < 0 || np.y >= N || np.x < 0 || np.x >= N) continue;
+		if (visited_value[MAP[np.y][np.x]]) continue;
+		if (visited[np.y][np.x]) continue;
+		if (MAP[np.y][np.x] == st_val) {
+			if (np.y != st.y || np.x != st.x) {
+				continue;
+			}
+		}
+
+		//cout << np.y << " " << np.x << "\n";
+		visited[np.y][np.x] = 1;
+		visited_value[MAP[np.y][np.x]] = 1;
+		
+		dfs(np, (dir + i)%4, cost + 1,st_val, rest_turn - i);
+		visited[np.y][np.x] = 0;
+		visited_value[MAP[np.y][np.x]] = 0;
+		//cout << np.y << " " << np.x << "\n";
+	}
+}
+
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int T;
+	cin >> T;
+
+	for (int test_case = 1; test_case <= T; test_case++) {
+		cin >> N;
+		max_cnt = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				cin >> MAP[i][j];
+			}
+		}
+
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				memset(visited, 0, sizeof(visited));
+				memset(visited_value, 0, sizeof(visited_value));
+				//cout << i <<" "<< j << "\n";
+				st = { i,j };
+				dfs(st, 0, 0, MAP[i][j],4);
+				//cout << "\n";
+			}
+		}
+		if (max_cnt == 0) cout << "#"<< test_case<<" "<< - 1 << "\n";
+		else cout <<"#" << test_case <<" "<< max_cnt << "\n";
+	}
+
+	return 0;
+}
